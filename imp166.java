e2f5129911986becbde21bdc85edc4628c5f2fb1
prachichandani <prachichandani05@gmail.com>
2024-08-20 18:42:47 +0530

    Update imp166.java

--- imp166.java
+++ imp166.java
@@ -1,9 +1,9 @@
-import java.util.*;
+import java.util.*; //chocola problem
 public class imp166 {
     public static void main(String args[]){
         int n =4; int m = 6;
         Integer costver [] = {2,1,3,1,4};
-        Integer costhor [] = {4,1,2};
+        Integer costhor [] = {4,1,2};,
         Arrays.sort(costver, Collections.reverseOrder());
         Arrays.sort(costhor, Collections.reverseOrder());
         int h =0;
